f7644099d4a86d7e9fab3e3fcfb4094fdc58f097
Himani Bhatnagar <himanibhatnagar9311@gmail.com>
2026-09-03 17:20:39 +0530

    Initial setup: added client and server directories

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is running successfully");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
